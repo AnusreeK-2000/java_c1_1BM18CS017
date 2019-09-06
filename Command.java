3e2a1cce714720cf12b94af50326c4af5828922e
AnusreeK-2000 <53806481+AnusreeK-2000@users.noreply.github.com>
2019-09-06 15:25:41 +0530

    Add files via upload

---
+++ Command.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Command
+{
+	public static void main(String args[])
+	{
+		int large=0;
+		for(int i=0;i<args.length;i++)
+		{
+			if(Integer.parseInt(args[i])>large)
+				large=Integer.parseInt(args[i]);
+		}
+		System.out.println("largest number="+large);
+	}
+}
